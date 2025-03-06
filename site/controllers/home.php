89631d40e3cab6a50c738a13308d01aad7a92692
Asaf Arad <asafarad1@gmail.com>
2025-03-06 18:00:10 +0200

    button

--- site/controllers/home.php
+++ site/controllers/home.php
@@ -16,6 +16,7 @@ return function ($site, $kirby) {
                         ]);
                     } catch (Exception $e) {
                     }
+                    go("/");
                 }
             }
         }
