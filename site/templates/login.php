c4dd23f42d8f5b99e7ff7414b74712fc3951c9b3
Asaf Arad <asafarad1@gmail.com>
2025-03-06 23:21:10 +0200

    login

--- site/templates/login.php
+++ site/templates/login.php
@@ -1,32 +1,39 @@
 <?php snippet('header') ?>
 
 <div class="intro">
-<h1><?= $page->title() ?></h1>
+    <h1><?= $page->title() ?></h1>
 </div>
 
 <div class="login">
     <div class="text">
-        <?php if(!empty( $alerts )): ?>
-            <?php foreach ( $alerts as $alert ) : ?>
-            <div class="alert"><?=$alert ?></div>
+    <div class="workshops-header">
+            <div class="workshops-header-label">
+                <img src="<?= asset("assets/images/logo_full.svg")->url() ?>" alt="pgmr.club-logo">
+            </div>
+        </div>
+        
+        <?php if (!empty($alerts)): ?>
+            <?php foreach ($alerts as $alert) : ?>
+                <div class="alert"><?= $alert ?></div>
             <?php endforeach; ?>
         <?php endif ?>
 
+
         <form method="post" action="<?= $page->url() ?>">
             <input type="hidden" name="csrf" value="<?= csrf() ?>">
-            <?php if($status->status() === 'inactive'): ?>
-            <div>
-                <label for="tel">מספר טלפון נייד:</label>
-                <input id="tel" name="tel" required type="tel" value="<?= esc($tel ?? "", 'attr') ?>">
-            </div>
+            <?php if ($status->status() === 'inactive'): ?>
+                <div>
+                    <label for="tel">מספר טלפון נייד:</label>
+                    <input id="tel" name="tel" required type="tel" value="<?= esc($tel ?? "", 'attr') ?>">
+                </div>
             <?php endif ?>
 
             <?php if ($status->status() === 'pending'): ?>
-            <div>
-                <label for="name">קוד כניסה</label>
-                <input id="code" name="code" placeholder="000 000" required type="text">
-                <p><small>אנא בדקו את תיבת המייל שלכם וכתבו את הקוד שקיבלתם, אם לא התקבל מייל בדקו בתיקיית הספאם.</small></p>
-            </div>
+                <div>
+                    <label for="name">קוד כניסה</label>
+                    <input id="code" name="code" placeholder="000 000" required type="text">
+                    <p><small>אנא בדקו את תיבת המייל שלכם וכתבו את הקוד שקיבלתם, אם לא התקבל מייל בדקו בתיקיית הספאם.</small></p>
+                </div>
             <?php endif ?>
             <div>
                 <input type="submit" name="login" value="כניסה">
@@ -34,7 +41,6 @@
 
         </form>
     </div>
-</article>
+    </article>
 
-<?php snippet('footer') ?>
-
+    <?php snippet('footer') ?>
