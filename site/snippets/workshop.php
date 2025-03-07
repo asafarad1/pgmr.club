f23ccf99b1db951e06b15cafdc5c8bbaa43d1b9d
Asaf Arad <asafarad1@gmail.com>
2025-03-07 11:49:41 +0200

    blank

--- site/snippets/workshop.php
+++ site/snippets/workshop.php
@@ -1,56 +1,77 @@
-<?php foreach ($workshops as $workshop): ?>
-    <?php if (!$workshop->participants()->toUsers()->has($kirby->user())) : ?>
-        <div class="workshop">
-        <?php else : ?>
-            <div class="workshop registered">
-            <?php endif; ?>
-            <div class="workshop-top">
-                <div class="workshop-title">
-                    <h2><?= $workshop->title() ?></h2>
-                </div>
-                <div class="workshop-description">
-                    <h2><?= $workshop->about() ?></h2>
-                </div>
-                <?php foreach ($workshop->teacher()->split() as $teacher): ?>
-                    <div class="workshop-teacher"><?= $teacher ?></div>
-                <?php endforeach ?>
-                <div class="workshop-rooms">
-                    <p><?= $workshop->rooms() ?></p>
-                </div>
-                <div class="workshop-time">
-                    <span><?= $workshop->start_time()->toDate('H:i') ?></span>
-                    <span>—</span>
-                    <span><?= $workshop->end_time()->toDate('H:i') ?></span>
-                </div>
-                <div class="workshop-tags">
-                    <?php foreach ($workshop->workshop_type()->split() as $type): ?>
-                        <div class="workshop-type"><?= $type ?></div>
+<?php if ($workshops->isNotEmpty()): ?>
+    <?php foreach ($workshops as $workshop): ?>
+        <?php if (!$workshop->participants()->toUsers()->has($kirby->user())) : ?>
+            <div class="workshop">
+            <?php else : ?>
+                <div class="workshop registered">
+                <?php endif; ?>
+                <div class="workshop-top">
+                    <div class="workshop-title">
+                        <h2><?= $workshop->title() ?></h2>
+                    </div>
+                    <div class="workshop-description">
+                        <h2><?= $workshop->about() ?></h2>
+                    </div>
+                    <?php foreach ($workshop->teacher()->split() as $teacher): ?>
+                        <div class="workshop-teacher"><?= $teacher ?></div>
                     <?php endforeach ?>
-                    <?php foreach ($workshop->target_audience()->split() as $audience): ?>
-                        <div class="workshop-Target-audience"><?= $audience ?></div>
-                    <?php endforeach ?>
+                    <div class="workshop-rooms">
+                        <p><?= $workshop->rooms() ?></p>
+                    </div>
+                    <div class="workshop-time">
+                        <span><?= $workshop->start_time()->toDate('H:i') ?></span>
+                        <span>—</span>
+                        <span><?= $workshop->end_time()->toDate('H:i') ?></span>
+                    </div>
+                    <div class="workshop-tags">
+                        <?php foreach ($workshop->workshop_type()->split() as $type): ?>
+                            <div class="workshop-type"><?= $type ?></div>
+                        <?php endforeach ?>
+                        <?php foreach ($workshop->target_audience()->split() as $audience): ?>
+                            <div class="workshop-Target-audience"><?= $audience ?></div>
+                        <?php endforeach ?>
+                    </div>
                 </div>
-            </div>
-            <?php if (!$workshop->participants()->toUsers()->has($kirby->user())) : ?>
-                <?php if ($workshop->getAvailability()) : ?>
+                <?php if (!$workshop->participants()->toUsers()->has($kirby->user())) : ?>
+                    <?php if ($workshop->getAvailability()) : ?>
+                        <form method="post">
+                            <input type="hidden" name="action" value="register">
+                            <input type="hidden" name="workshop_id" value="<?= $workshop->id() ?>">
+                            <button type="submit" class="apply-button register">
+                                <h2>הרשמה</h2>
+                            </button>
+                        </form>
+                    <?php else : ?>
+                        No room left.
+                    <?php endif; ?>
+                <?php else : ?>
                     <form method="post">
-                        <input type="hidden" name="action" value="register">
+                        <input type="hidden" name="action" value="unregister">
                         <input type="hidden" name="workshop_id" value="<?= $workshop->id() ?>">
-                        <button type="submit" class="apply-button register">
-                            <h2>הרשמה</h2>
+                        <button type="submit" class="apply-button unregister">
+                            <h2>נרשמתי &#128527;</h2>
                         </button>
                     </form>
-                <?php else : ?>
-                    No room left.
                 <?php endif; ?>
-            <?php else : ?>
-                <form method="post">
-                    <input type="hidden" name="action" value="unregister">
-                    <input type="hidden" name="workshop_id" value="<?= $workshop->id() ?>">
-                    <button type="submit" class="apply-button unregister">
-                        <h2>נרשמתי &#128527;</h2>
-                    </button>
-                </form>
-            <?php endif; ?>
+                </div>
+            <?php endforeach ?>
+        <?php else : ?>
+            <div class="workshop blank">
+                <div class="workshop-top">
+                    <div class="workshop-title">
+                        <h2>יפורסם בקרוב</h2>
+                    </div>
+                    <div class="blank-rect"></div>
+                    <div class="blank-rect"></div>
+                    <div class="blank-sep"></div>
+                    <div class="blank-rect"></div>
+                    <div class="blank-sep"></div>
+                    <div class="blank-tags">
+                        <div class="blank-tag-type"></div>
+                        <div class="blank-tag-Target-audience"></div>
+                    </div>
+                    <div class="blank-sep"></div>
+                </div>
+                <div class="apply-button"></div>
             </div>
-        <?php endforeach ?>
+        <?php endif; ?>
